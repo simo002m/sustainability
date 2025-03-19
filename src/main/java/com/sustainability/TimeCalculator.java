3a9abfca6159cd305b81a00d1fc0cf3b42a89546
NajaJakobsen <najafj@me.com>
2025-03-19 20:51:38 +0100

    Lavet udregning af tiden det tager at hente affald på standardruten ud fra hvor mange steder de skal hen.

--- src/main/java/com/sustainability/TimeCalculator.java
+++ src/main/java/com/sustainability/TimeCalculator.java
@@ -7,22 +7,24 @@ public class TimeCalculator {
     private int numberOfLocations;
     private final double timeBetweenHouse = 0.25;
     private double totalTimeInMinutes;
-    private final int timeForStandardRoute = 87; // calcuated to be 87minutes
+    private final int timeForStandardRouteWithoutBinsCollected;
 
-    //standard route plus bins = 2h 15min should be a variable to use it in the graph.
+    // Standard route without bins collected = 1h 27min.
+    // Standard route with 50 bins collected on all 5 locations = 2h 15min. Should be a variable to use it in the graph.
 
     /**
      * this is a time caluculator for the route
      * @param numberOfLocations how many places where garbage need to be collected.
      * @param bin number of bins that need to get collected.
      */
-    public TimeCalculator(int numberOfLocations, int bin) {
+    public TimeCalculator(int numberOfLocations, int bin, int timeForStandardRouteWithoutBinsCollected) {
        this.numberOfLocations = numberOfLocations;
        this.bin = bin;
+       this.timeForStandardRouteWithoutBinsCollected = timeForStandardRouteWithoutBinsCollected;
     }
 
     public double getTotalTimeInMinutes() {
-        totalTimeInMinutes= (bin * timePerBinAndBetweenHouse - (timeBetweenHouse * numberOfLocations) + timeForStandardRoute);
+        totalTimeInMinutes = (bin * timePerBinAndBetweenHouse - (timeBetweenHouse * numberOfLocations) + timeForStandardRouteWithoutBinsCollected);
         return totalTimeInMinutes;
     }
 }
