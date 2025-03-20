79d985cf82e57bd4deacb34abb0bbc40bec634e9
simo002m <simo002m@gmail.com>
2025-03-20 01:05:53 +0100

    GraphsController

--- src/main/java/com/sustainability/GraphsController.java
+++ src/main/java/com/sustainability/GraphsController.java
@@ -15,10 +15,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class SolarController {
-    private ArrayList<> data;
+public class GraphsController {
+    private ArrayList<Measurement> data;
 
-    //region - Variables for scene builder controls
     @FXML
     private Label errorMessage; // Label to show error message when choosing invalid choice
     @FXML
@@ -26,41 +25,26 @@ public class SolarController {
     @FXML
     private DatePicker dateDP; // DP: Date Picker
     @FXML
-    private TextField diagramTypeDDL; // Shows which chart type is showing
+    private LineChart<String, Integer> monthGraph; // Day chart
     @FXML
-    private BarChart<String, Integer> productionBarChart; // Day chart
-    @FXML
-    private LineChart<String, Integer> productionLineChart; // Month chart
-    @FXML
-    private Label productionTotal; // Label to calculate kWh
-    //endregion
-
+    private LineChart<String, Integer> yearGraph; // Month chart
 
     // Initializes and reads the data file
     @FXML
     public void initialize() {
-        try {
-            data = ReadData.readFileData("src/resources/solar-dataset.tsv");
-
-            for (SolarData solarData : data) {
+            for (Measurement measurement : data) {
                 // finds all siteIDs that are not currently added to siteDDL
-                if (!siteDDL.getItems().contains(String.valueOf(solarData.getSiteID()))) {
+                if (!siteDDL.getItems().contains(String.valueOf(measurement.getID()))) {
                     // convert int to String, since choicebox only takes Strings
-                    siteDDL.getItems().add(String.valueOf(solarData.getSiteID()));
+                    siteDDL.getItems().add(String.valueOf(measurement.getID()));
                 }
             }
 
-            siteDDL.setValue(String.valueOf(data.get(0).getSiteID()));
+            siteDDL.setValue(String.valueOf(data.get(0).getID()));
 
             // Ensure both charts start hidden
-            productionBarChart.setVisible(false);
-            productionLineChart.setVisible(false);
-
-        } catch (FileNotFoundException e) {
-            errorMessage.setText("File not found");
-        } catch (Exception e) {
-            errorMessage.setText(e.getMessage());
-        }
+            monthGraph.setVisible(false);
+            yearGraph.setVisible(false);
     }
 
     // The on action for the search button
@@ -105,26 +89,15 @@ public class SolarController {
         if (totalsWhs.isEmpty() && dailyTotals.isEmpty()) {
             errorMessage.setText("No data for chosen date or month.");
         } else {
-            createDayChart(siteIDPicked, datePicked, totalsWhs, times);
-            createMonthChart(siteIDPicked, datePicked, dailyTotals);
+            createMonthGraph(siteIDPicked, datePicked, totalsWhs, times);
+            createYearGraph(siteIDPicked, datePicked, dailyTotals);
         }
 
-        // Show only the selected chart type
-        switch (diagramTypeDDL.getText()) {
-            case "Bar Chart": // Shows day graph
-                productionBarChart.setVisible(true);
-                productionLineChart.setVisible(false);
-                break;
-            case "Line Chart": // Shows month graph
-                productionBarChart.setVisible(false);
-                productionLineChart.setVisible(true);
-                break;
-            default:
-        }
+
     }
 
     // Create the day chart
-    public void createDayChart(int siteIDPicked, LocalDate datePicked, ArrayList<Integer> totalWhs, ArrayList<Integer> times) {
+    public void createMonthGraph(int siteIDPicked, LocalDate datePicked, ArrayList<Integer> totalWhs, ArrayList<Integer> times) {
         XYChart.Series<String, Integer> series = new XYChart.Series<>();
         series.setName("Site ID: " + siteIDPicked + "\nDate: " + datePicked.toString());
 
@@ -132,11 +105,11 @@ public class SolarController {
             series.getData().add(new XYChart.Data<>(times.get(i) + ":00", totalWhs.get(i)));
         }
 
-        productionBarChart.setData(FXCollections.observableArrayList(series));
+        monthGraph.setData(FXCollections.observableArrayList(series));
     }
 
     // Create the month chart
-    public void createMonthChart(int siteIDPicked, LocalDate datePicked, HashMap<Integer, Integer> dailyTotals) {
+    public void createYearGraph(int siteIDPicked, LocalDate datePicked, HashMap<Integer, Integer> dailyTotals) {
         XYChart.Series<String, Integer> series = new XYChart.Series<>();
         series.setName("Site ID: " + siteIDPicked + "\nMonth: " + datePicked.getMonth() + " " + datePicked.getYear());
 
@@ -144,23 +117,21 @@ public class SolarController {
             series.getData().add(new XYChart.Data<>("Day " + day, dailyTotals.get(day)));
         }
 
-        productionLineChart.setData(FXCollections.observableArrayList(series));
+        yearGraph.setData(FXCollections.observableArrayList(series));
     }
 
     // Button to show the day chart
-    public void showDayChart()
+    public void showMonthGraph()
     {
-        diagramTypeDDL.setText("Bar Chart");
-        productionBarChart.setVisible(true);
-        productionLineChart.setVisible(false);
+        monthGraph.setVisible(true);
+        yearGraph.setVisible(false);
     }
 
     // Button to show the month chart
-    public void showMonthChart()
+    public void showYearGraph()
     {
-        diagramTypeDDL.setText("Line Chart");
-        productionBarChart.setVisible(false);
-        productionLineChart.setVisible(true);
+        monthGraph.setVisible(false);
+        yearGraph.setVisible(true);
     }
 
 }
